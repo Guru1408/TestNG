93f9d32a4d4870119d17da93e75cedaedc23af9c
Your Name <you@example.com>
2024-09-10 03:09:03 +0530

    TestNG

---
+++ src/test/java/com/training/testNG/NewTest2.java
@@ -0,0 +1,11 @@
+package com.training.testNG;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 extends NewTest3{
+  @Test
+  
+  public void test1() {
+	  System.out.println("this is my second testng project");
+  }
+}
---
+++ src/test/java/com/training/testNG/NewTest3.java
@@ -0,0 +1,51 @@
+package com.training.testNG;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest3 {
+	@BeforeClass
+	public void Beforeclass() {
+		System.out.println("before class");
+	}
+	@AfterClass
+	public void Afterclass() {
+		System.out.println("After class");
+		
+	}
+	@BeforeTest
+	public void Beforetest() {
+	   System.out.println("before Test");
+	}
+	@AfterTest
+	public void Aftertest() {
+		System.out.println("After test");
+		
+	}
+	@BeforeSuite
+	public void Beforesuite() {
+		System.out.println("before suite");
+	}
+	@AfterSuite
+	public void Aftersuite() {
+		System.out.println("after suite");
+	}
+@BeforeMethod
+	
+	public void beforemethod() {
+		System.out.println("before method");
+	}
+
+	@AfterMethod
+	  
+	  public void aftermethod() {
+			System.out.println("after  method");
+	  }
+}
